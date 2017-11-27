b3b99dc21e58c3e57c1035cbad429730dc50fc65
zz6868 <33901501+zz6868@users.noreply.github.com>
2017-11-27 10:35:46 +0800

    keep on optimizing
    school official net

---
+++ js/1.js
@@ -0,0 +1,61 @@
+var num = 0;
+     var abtn = document.getElementsByTagName('a')
+     var aold = abtn[num];
+	     aold.className ="java_first";
+	 var aimg = document.getElementsByTagName('img');
+	 var oldimg = aimg[num];
+	     oldimg.className ="firstimg";
+     var timer;
+
+	 for(var i=0;i<5;i++){
+		 abtn[i].index = i;//使下标对应
+	 abtn[i].onclick = function(){
+          
+		  
+	      clearInterval(timer);
+          num = this.index;
+		  run();
+	};
+  };
+  function run(){
+          aold.className = '';
+          aold = abtn[num];//更新aold
+	      abtn[num].className ="java_first";
+		  oldimg.className = '';
+		  oldimg = aimg[num]; //更新aimg
+		  aimg[num].className = "firstimg";
+		  
+  };
+      timer = setInterval(function(){
+		  num++;
+		  if(num==5){
+		  num=0;
+		  };
+	     run();
+	  
+	  },1000)
+	  
+	//鼠标移入显示左右按钮
+		$(".banner").hover(function(){
+			$(".btn").show();
+		},function(){
+			$(".btn").hide();
+		})
+    //点击左按钮
+		$(".left").click(function(){
+           clearInterval(timer);
+		   num--;
+		    if(num<0){
+		        num=4;
+		  };
+		run();
+     });
+	 //点击右按钮
+        $(".right").click(function(){
+             clearInterval(timer);
+		     num++;
+		     if(num>4){
+		     num=0;
+		  };
+		run();
+     });
